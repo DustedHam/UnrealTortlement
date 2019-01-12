dabc483a6ec3f1acb127b4f7e3c0b120e80a7755
DustedHam <JonJPGardner@Gmail.com>
2019-01-11 23:01:43 -0400

    Added more gun assets. started on bullets.

---
+++ Assets/UnrealTortlement/Projectiles/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnrealTortlement.projectiles
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody _rigidbody;
+
+        private ProjectilePool pool;
+
+        public void init(ProjectilePool pool, Vector3 position, Vector3 velocity)
+        {
+            this.pool = pool;
+            _rigidbody.position = position;
+            _rigidbody.velocity = velocity;
+        }
+    }
+}
---
+++ Assets/UnrealTortlement/Projectiles/ProjectilePool.cs
@@ -0,0 +1,39 @@
+﻿using SaltboxGames.Common.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnrealTortlement.projectiles
+{
+    public class ProjectilePool
+    {
+        public GameObject prefab;
+        private Pool<Projectile> pool;
+
+        private void Awake()
+        {
+            pool = new Pool<Projectile>(createBullet);
+        }
+
+        public Projectile spawn(Vector3 position, Vector3 velocity)
+        {
+            Projectile projectile = pool.Take();
+            projectile.gameObject.SetActive(true);
+            projectile.init(this, position, velocity);
+            return projectile;
+        }
+
+        public void despawn(Projectile projectile)
+        {
+            projectile.gameObject.SetActive(false);
+            pool.Add(projectile);
+        }
+
+        private Projectile createBullet()
+        {
+            GameObject obj = GameObject.Instantiate(prefab);
+            Projectile projectile = obj.GetComponent<Projectile>();
+            return projectile;
+        }
+    }
+}
