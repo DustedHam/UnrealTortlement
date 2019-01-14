0c126f1a9d959257bf4c36cbe4c84726fa319bda
oconnelljd1 <oconnelljd1@gmail.com>
2019-01-14 00:38:38 -0400

    Victory Scene

---
+++ Assets/UnrealTortlement/VictoryScene/VictoryScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnrealTortlement.Weapons;
+using UnityEngine.SceneManagement;
+
+public class VictoryScene : MonoBehaviour
+{
+
+    public Weapon ak;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("VictoryCoroutine");
+    }
+
+    public void ChangeScene(string scene)
+    {
+        StopCoroutine("VictoryCoroutine");
+        SceneManager.LoadScene(scene);
+    }
+
+    private IEnumerator VictoryCoroutine()
+    {
+        while(true)
+        {
+            yield return null;
+            ak._ammoCount = 3;
+            ak.tryFire("");
+        }
+    }
+}
